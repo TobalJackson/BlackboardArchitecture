2ba7c8f0e99bb4cce4be0ab85dd70cb0fd6dad99
Chris James <tobaljackson@gmail.com>
2015-03-23 01:36:45 -0400

    baseline functional

--- BlackBoardWilderness.py
+++ BlackBoardWilderness.py
@@ -31,14 +31,45 @@ from Expert import Expert
 #   Hunter: expresses insistence('I can hunt meat')
 #   Survivalist: expresses insistence('I can get fresh water/veggies')
 
-blackboard = {"tasks":{}, "inventory":[], "needs":[]}
+blackboard = {"inventory":[], "needs":[]}
 
 
 
-situation = "The travelers have taken a wrong turn and have ended up lost in The Great Forest of Ashwood. It has been nearly 8 hours since they've seen any sign of the designated trail, and they're beginning to get hungry." 
+print("\nThe travelers have taken a wrong turn and have ended up lost in The Great Forest of Ashwood.  At first they're focused on just finding their way.")
+chef = Expert("Chef", blackboard, cooking=0.99)
+hunter = Expert("Elmer", blackboard, hunting=0.99)
+lumberjack = Expert("Paul", blackboard, chopping=0.99)
+survivalist = Expert("Bear", blackboard, survivalism=0.99)
 
-chef = Expert("Chef",cooking=0.99)
-hunter = Expert("Elmer", hunting=0.99)
-lumberjack = Expert("Paul", chopping=0.99)
-survivalist = Expert("Bear", survivalism=0.99)
+experts = [chef, hunter, lumberjack, survivalist]
+for expert in experts:
+    print("{}'s insistence: {:1.3f}".format(expert.name, expert.getInsistence()))
+
+print("\nIt has been nearly 8 hours since they've seen any sign of the designated trail, and they're beginning to get hungry.")
+
+blackboard["needs"].append("food")
+
+lastExpert = None
+lastInsistence = None
+
+while "food" in blackboard["needs"] and "food" not in blackboard["inventory"]:
+    maxInsistence = 0
+    bestExpert = None
+    for expert in experts:
+        if expert == lastExpert:
+            continue
+        insistence = expert.getInsistence()
+        print("\t{}'s insistence: {:1.3f}".format(expert.name, insistence))
+        if insistence > maxInsistence:
+            maxInsistence = insistence
+            bestExpert = expert
+    
+    if bestExpert is not None:
+        print("{} has something to contribute to the group!".format(bestExpert.name))
+        bestExpert.run(blackboard)
+        lastExpert = bestExpert
+    for item in blackboard["inventory"]:
+        if item in blackboard["needs"]:
+            blackboard["needs"].remove(item)
+    print("\n\tInventory: {}\n\tNeeds: {}".format(blackboard["inventory"], blackboard["needs"]))
 
--- Expert.py
+++ Expert.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import random
+import operator
+
 class Expert:
     def __init__(self, name, blackboard, hunger=50, health=50, cooking=0.25, hunting=0.25, chopping=0.25, survivalism=0.25, variability=0.1):
         self.blackboard = blackboard
@@ -13,78 +16,131 @@ class Expert:
         self.chopping = chopping - (random.random() * variability)
         self.survivalism = survivalism  - (random.random() * variability)
 
-        self.skills = {"cooking":self.cooking, "hunting":self.hunting, "chopping":self.chopping, "locate":self.locate}
+        self.skills = {"cooking":self.cooking, "hunting":self.hunting, "chopping":self.chopping, "survivalism":self.survivalism}
+        self.sortedSkills = sorted(self.skills.items(), key=operator.itemgetter(1), reverse=True)
 
-        self.inventory = blackboard["inventory"]
+        #self.inventory = blackboard["inventory"]
         self.tasks = []
 
-    def executeTasks(self):
-        for task in self.tasks:
-            if (task == "cook"):
-                if "ingredients" in inventory:
-                    if(cook(food)):
-                        blackboard["inventory"].append("food")
-                    blackboard["inventory"].remove("ingredients")
-                elif ("ingredients" not in blackboard["needs"]:
-                        blackboard["needs"].append("ingredients") = []
-        
-
     def cook(self, food_difficulty=1.0):
-        if "food" in inventory:
+        if "food" in self.blackboard["inventory"]:
             print("We already have food!")
             return
-        if "meat" in inventory and "water" in inventory and "fire" in inventory:
+        if "meat" in self.blackboard["inventory"] and "water" in self.blackboard["inventory"] and "fire" in self.blackboard["inventory"]:
             difficulty = self.cooking / food_difficulty
-            result = random.random() < diffuculty
+            result = random.random() < difficulty
             if (result):
-                print("I have successuflly cooked food. Enjoy!".format(food.string))
-                    blackboard["inventory"].append("food")
+                print("I have successuflly cooked the food. Enjoy!")
+                self.blackboard["inventory"].append("food")
+                self.blackboard["needs"].remove("food")
             else:
                 print("I screwed this dish up.  We could eat it at the risk of...Dysentery")
-            blackboard["inventory"].remove("meat")
-            blackboard["inventory"].remove("water")
-            blackboard["inventory"].remove("fire")
+                self.blackboard["inventory"].remove("meat")
+                self.blackboard["inventory"].remove("water")
+                self.blackboard["inventory"].remove("fire")
         else:
-            if "meat" not in inventory:
-                blackboard["needs"].append("meat")
-                print("We need Meat!")
-            if "water" not in inventory:
-                blackboard["needs"].append("water")
-                print("We need Water!")
-            if "fire" not in inventory:
-                blackboard["needs"].append("fire")
-                print("We need Fire!")
+            if "meat" not in self.blackboard["inventory"]:
+                self.blackboard["needs"].append("meat")
+                print("Chef: We need Meat!")
+            if "water" not in self.blackboard["inventory"]:
+                self.blackboard["needs"].append("water")
+                print("Chef: We need Water!")
+            if "fire" not in self.blackboard["inventory"]:
+                self.blackboard["needs"].append("fire")
+                print("Chef: We need Fire!")
 
     def hunt(self, animal_difficulty=1.0):
+        inventory = self.blackboard["inventory"]
+        needs = self.blackboard["needs"]
         difficulty = self.hunting / animal_difficulty
         result = random.random() < difficulty
-        if (result):
-            print("I have successfully hunted {}.  Let's cook it!".format(animal.string))
+        if "meat" in inventory:
+            print("{}: We already have meat!".format(self.name))
+            if "meat" in needs:
+                needs.remove("meat")
+            return
+        elif (result):
+            print("{}:I have successfully hunted for meat!  Let's cook it!".format(self.name))
             inventory.append("meat")
+            needs.remove("meat")
         else:
-            print("I have failed this hunting mission.  Shall we eat our boots?")
+            print("{}: I have failed this hunting mission.  Shall we eat our boots?".format(self.name))
 
-    def chop(self wood_difficulty):
+    def chop(self, wood_difficulty=1.0):
         difficulty = self.chopping  / wood_difficulty
         result = random.random() < difficulty
-        if (result):
-            print("I have chopped the {}.  Lets make something with this fine wood".format(wood.string))
+        if "wood" in self.blackboard["inventory"]:
+            print("{}: We already have some wood!".format(self.name))
+            return
+        elif (result):
+            print("{}: I have chopped the wood.  Lets make something with this fine wood".format(self.name))
+            self.blackboard["inventory"].append("wood")
+            self.blackboard["needs"].remove("wood")
         else:
-            print("I have failed miserably and my axe has broken!")
+            print("{}: I have failed miserably! We must try again to get wood.".format(self.name))
         return result
 
-    def survival(self resource_difficulty):
-        difficulty = self.locate  / resource_difficulty
+    def survival(self, resource_difficulty=1.0):
+        difficulty = self.survivalism  / resource_difficulty
         result = random.random() < difficulty
-        if (result):
-            print("I have found some {}. Let's enjoy it while it lasts..".format(item.string))
+        if "water" in self.blackboard["inventory"] and "fire" in self.blackboard["inventory"]:
+            print("{}: We already have water and Fire.  Better drink my own piss...".format(self.name))
+        elif "water" in self.blackboard["needs"]:
+            if (result):
+                print("{}: I have found some water! Let's enjoy it while it lasts..".format(self.name))
+                self.blackboard["inventory"].append("water")
+                self.blackboard["needs"].remove("water")
+            else:
+                print("{}: I've failed to find any water.  We can all drink my piss...".format(self.name))
+        elif "fire" in self.blackboard["needs"]:
+            if "wood" in self.blackboard["inventory"]:
+                if (result):
+                    print("{}: I have made a fire! Let us bask in its warmth and/or use it to cook!".format(self.name))
+                    self.blackboard["inventory"].append("fire")
+                    self.blackboard["needs"].remove("fire")
+                    self.blackboard["inventory"].remove("wood")
+                else:
+                    print("{}: I have screwed up making the fire! We need more wood...".format(self.name))
+                    self.blackboard["inventory"].remove("wood")
+            else:
+                print("{}: We need wood to make a fire!".format(self.name))
+                self.blackboard["needs"].append("wood")
+            
 
-    def evaluateBlackboard(self, blackboard):
-        for task in blackboard["tasks"]:
-            if (task in ["cooking", "hunting", "chopping", "locate"]):
-                blackboard["tasks"][task].append((self, interest(task)))
-        for item in blackboard["inventory"]:
-            self.inventory.append(item)
 
-    def interest(task):
-        return self.skills[task]
+    def run(self, blackboard):
+        myStrength = self.sortedSkills[0][0]
+        if myStrength == "cooking":
+            self.cook()
+        if myStrength == "chopping":
+            self.chop()
+        if myStrength == "hunting":
+            self.hunt()
+        if myStrength == "survivalism":
+            self.survival()
+
+
+
+    def getInsistence(self):
+        Insistences = [0]
+        if "water" in self.blackboard["needs"]:
+            Insistences.append(self.survivalism)
+        if "fire" in self.blackboard["needs"]:
+            insistence = self.survivalism
+            if "wood" not in self.blackboard["inventory"]:
+                insistence = insistence - 0.25
+            Insistences.append(insistence)
+        if "meat" in self.blackboard["needs"]:
+            Insistences.append(self.hunting)
+        if "food" in self.blackboard["needs"]:
+            insistence = self.cooking
+            if "water" not in self.blackboard["inventory"]:
+                insistence = insistence - 0.25
+            if "meat" not in self.blackboard["inventory"]:
+                insistence = insistence - 0.25
+            if "fire" not in self.blackboard["inventory"]:
+                insistence = insistence - 0.25
+            Insistences.append(insistence)
+        if "wood" in self.blackboard["needs"]:
+            Insistences.append(self.chopping)
+        return sorted(Insistences, reverse=True)[0]
